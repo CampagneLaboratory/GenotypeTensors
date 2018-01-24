51bd0ffe69f20df2536008e101c558d96154b1f3
Joshua Cohen <jbc264@cornell.edu>
2018-01-24 13:03:08 -0500

    Modify getitem to properly parse tuples from VectorReader

--- src/org/campagnelab/dl/genotypetensors/genotype_pytorch_dataset.py
+++ src/org/campagnelab/dl/genotypetensors/genotype_pytorch_dataset.py
@@ -8,16 +8,18 @@ class GenotypeDataset(Dataset):
     """" Implement a dataset that can be traversed only once, in increasing and contiguous index number."""
     def __init__(self, vec_basename, sample_name, vector_names):
         # initialize vector reader with basename and selected vectors:
-        self.reader=VectorReader(vec_basename, sample_name=sample_name, vector_names=vector_names, return_example_id=True)
-        self.props=self.reader.vector_reader_properties
+        self.reader = VectorReader(vec_basename, sample_name=sample_name, vector_names=vector_names,
+                                 return_example_id=True)
+        self.props = self.reader.vector_reader_properties
         # obtain number of examples from .vecp file:
-        self.length=self.props.get_num_records()
+        self.length = self.props.get_num_records()
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-        # get next example from .vec file and check that idx matches example index, then return the features and outputs as tuple.
-        example_index, tuple= next(self.reader)
-        assert example_index==idx, "Requested example index out of order of .vec file."
-        return tuple
+        # get next example from .vec file and check that idx matches example index,
+        # then return the features and outputs as tuple.
+        example_tuple = next(self.reader)
+        assert example_tuple[0] == idx, "Requested example index out of order of .vec file."
+        return example_tuple[1:]
