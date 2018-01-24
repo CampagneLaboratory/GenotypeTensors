bcdaf5f0308df06b8b375ed139de972f2c9da28d
Joshua Cohen <jbc264@cornell.edu>
2018-01-24 13:15:31 -0500

    Fix import and call to constructor of VectorReader in GenotypeDataset

--- src/org/campagnelab/dl/genotypetensors/genotype_pytorch_dataset.py
+++ src/org/campagnelab/dl/genotypetensors/genotype_pytorch_dataset.py
@@ -1,15 +1,15 @@
-
 from torchnet.dataset.dataset import Dataset
 
-from org.campagnelab.dl.genotypetensors import VectorReader
+from org.campagnelab.dl.genotypetensors.VectorReader import VectorReader
 
 
 class GenotypeDataset(Dataset):
     """" Implement a dataset that can be traversed only once, in increasing and contiguous index number."""
-    def __init__(self, vec_basename, sample_name, vector_names):
+    def __init__(self, vec_basename, sample_id, vector_names):
+        super().__init__()
         # initialize vector reader with basename and selected vectors:
-        self.reader = VectorReader(vec_basename, sample_name=sample_name, vector_names=vector_names,
-                                 return_example_id=True)
+        self.reader = VectorReader(vec_basename, sample_id=sample_id, vector_names=vector_names,
+                                   return_example_id=True)
         self.props = self.reader.vector_reader_properties
         # obtain number of examples from .vecp file:
         self.length = self.props.get_num_records()
