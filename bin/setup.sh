7d5ecdfb7f116db1989b11f72cf3ade85256690d
Fabien Campagne <fac2003@campagnelab.org>
2018-01-25 12:31:48 -0500

    Fix script.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+#set -x
 export GENOTYPE_TENSORS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-export PYTHONPATH="${PYTHONPATH}:${GENOTYPETENSORS}/src"
+export PYTHONPATH="${PYTHONPATH}:${GENOTYPE_TENSORS}/src"
 if [[ $OSTYPE =="cygwin" ]]; then
     GENOTYPE_TENSORS=`cygpath -m "${GENOTYPE_TENSORS}"`
     PYTHONPATH=`cygpath -m "${PYTHONPATH}"`
