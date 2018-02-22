f41c72bfec5a3ba92bcfdbdf669c7a1ce74641a9
Fabien Campagne <fac2003@campagnelab.org>
2018-02-22 09:52:47 -0500

    Fix weight problem.

--- src/org/campagnelab/dl/genotypetensors/autoencoder/genotyping_supervised_trainer.py
+++ src/org/campagnelab/dl/genotypetensors/autoencoder/genotyping_supervised_trainer.py
@@ -46,7 +46,7 @@ class GenotypingSupervisedTrainer(CommonTrainer):
 
 
         class_frequencies=class_frequencies+1
-        weights=1.0-torch.norm(class_frequencies, p=1, dim=0)
+        weights=torch.ones(class_frequencies.size())-torch.norm(class_frequencies, p=1, dim=0)
         if self.use_cuda:
             weights=weights.cuda()
         self.criterion_classifier = CrossEntropyLoss(weight=weights)
