b7d16ed86dd838b9a42c2f5826bbb9274fff7413
Joshua Cohen <jbc264@cornell.edu>
2018-03-07 23:57:38 -0500

    Draft of encoder, decoder, and discriminator networks for semisupervised autoencoder. Still need to integrate into rest of training infrastructure, including sampling from prior.

---
+++ src/org/campagnelab/dl/genotypetensors/autoencoder/semisup_adversarial_autoencoder.py
@@ -0,0 +1,163 @@
+from torch import nn
+
+class SemiSupAdvEncoder(nn.Module):
+    def __init__(self, input_size=512, N_dim=500, ngpus=1, dropout_p=0, num_hidden_layers=3, num_classes=10, z_dim=2):
+        super().__init__()
+        self.ngpus = ngpus
+        self.device_list = list(range(self.ngpus))
+
+        # Create encoders
+        encoder_list = [
+            nn.BatchNorm1d(input_size),
+            nn.Linear(input_size, N_dim),
+            nn.Dropout(dropout_p),
+            nn.SELU()
+        ]
+        for _ in range(num_hidden_layers):
+            encoder_list += [
+                nn.BatchNorm1d(N_dim),
+                nn.Linear(N_dim, N_dim),
+                nn.Dropout(dropout_p),
+                nn.SELU()
+            ]
+        self.encoder_base = nn.Sequential(*encoder_list)
+        self.cat_encoder = nn.Linear(N_dim, num_classes)
+        self.prior_encoder = nn.Linear(N_dim, z_dim)
+    
+    def forward(self, input):
+        if input.data.is_cuda and self.ngpus > 1:
+            cat_encoded = nn.parallel.data_parallel(self.cat_encoder, input, self.device_list)
+            prior_encoded = nn.parallel.data_parallel(self.prior_encoder, input, self.device_list)
+        else:
+            cat_encoded = self.cat_encoder(input)
+            prior_encoded = self.prior_encoder(input)
+        return cat_encoded, prior_encoded
+
+
+class SemiSupAdvDecoder(nn.Module):
+    def __init__(self, input_size=512, N_dim=500, ngpus=1, dropout_p=0, num_hidden_layers=3, num_classes=10, z_dim=2):
+        super().__init__()
+        self.ngpus = ngpus
+        self.device_list = list(range(self.ngpus))
+
+        # Create decoder
+        decoder_list = [
+            nn.BatchNorm1d(z_dim + num_classes),
+            nn.Linear(z_dim + num_classes, N_dim),
+            nn.Dropout(dropout_p),
+            nn.SELU(),
+            nn.BatchNorm1d()
+        ]
+        for _ in range(num_hidden_layers):
+            decoder_list += [
+                nn.BatchNorm1d(N_dim),
+                nn.Linear(N_dim, N_dim),
+                nn.Dropout(dropout_p),
+                nn.SELU()
+            ]
+        decoder_list += [
+            nn.BatchNorm1d(N_dim),
+            nn.Linear(N_dim, input_size)
+        ]
+        self.decoder = nn.Sequential(*decoder_list)
+    
+    def forward(self, input):
+        if input.data.is_cuda and self.ngpus > 1:
+            decoded = nn.parallel.data_parallel(self.decoder, input, self.device_list)
+        else:
+            decoded = self.decoder(input)
+        return decoded
+
+
+class SemiSupAdvDiscriminatorCat(nn.Module):
+    def __init__(self, N_dim=500, ngpus=1, dropout_p=0, num_hidden_layers=3, num_classes=10):
+        super().__init__()
+        self.ngpus = ngpus
+        self.device_list = list(range(self.ngpus))
+
+        layer_list = [
+            nn.BatchNorm1d(num_classes),
+            nn.Linear(num_classes, N_dim),
+            nn.Dropout(dropout_p),
+            nn.SELU()
+        ]
+
+        for _ in range(num_hidden_layers):
+            layer_list += [
+                nn.BatchNorm1d(N_dim),
+                nn.Linear(N_dim, N_dim),
+                nn.Dropout(dropout_p),
+                nn.SELU()
+            ]
+        
+        layer_list += [
+            nn.BatchNorm1d(N_dim),
+            nn.Linear(N_dim, 1)
+            nn.Sigmoid()
+        ]
+        self.cat_discriminator = nn.Sequential(*self.layer_list)
+    
+    def forward(self, input):
+        if input.data.is_cuda and self.ngpus > 1:
+            category = nn.parallel.data_parallel(self.cat_discriminator, input, self.device_list)
+        else:
+            category = self.cat_discriminator(input)
+        return category
+
+
+class SemiSupAdvDiscriminatorPrior(nn.Module):
+    def __init__(self, N_dim=500, ngpus=1, dropout_p=0, num_hidden_layers=3, z_dim=2):
+        super().__init__()
+        self.ngpus = ngpus
+        self.device_list = list(range(self.ngpus))
+
+        layer_list = [
+            nn.BatchNorm1d(num_classes),
+            nn.Linear(z_dim, N_dim),
+            nn.Dropout(dropout_p),
+            nn.SELU()
+        ]
+
+        for _ in range(num_hidden_layers):
+            layer_list += [
+                nn.BatchNorm1d(N_dim),
+                nn.Linear(N_dim, N_dim),
+                nn.Dropout(dropout_p),
+                nn.SELU()
+            ]
+        
+        layer_list += [
+            nn.BatchNorm1d(N_dim),
+            nn.Linear(N_dim, 1)
+            nn.Sigmoid()
+        ]
+        self.prior_discriminator = nn.Sequential(*self.layer_list)
+    
+    def forward(self, input):
+        if input.data.is_cuda and self.ngpus > 1:
+            prior_val = nn.parallel.data_parallel(self.prior_discriminator, input, self.device_list)
+        else:
+            prior_val = self.prior_discriminator(input)
+        return prior_val
+
+def create_semisup_adv_autoencoder_model(model_name, problem, encoded_size=32, ngpus=1, nreplicas=1, dropout_p=0, N_dim=500,
+        num_hidden_layers=1, z_dim=2):
+    input_size = problem.input_size("input")
+    assert len(input_size) == 1, "AutoEncoders required 1D input features."
+
+    encoder = SemiSupAdvEncoder(input_size=input_size[0], N_dim=N_dim, ngpus=ngpus, dropout_p=dropout_p,
+        num_hidden_layers=num_hidden_layers, num_classes=encoded_size, z_dim=z_dim)
+    decoder = SemiSupAdvDecoder(input_size=input_size[0], N_dim=N_dim, ngpus=ngpus, dropout_p=dropout_p,
+        num_hidden_layers=num_hidden_layers, num_classes=encoded_size, z_dim=z_dim)
+    discriminator_cat = SemiSupAdvDiscriminatorCat(N_dim=N_dim, ngpus=ngpus, dropout_p=dropout_p,
+        num_hidden_layers=num_hidden_layers, num_classes=encoded_size)
+    discriminator_prior = SemiSupAdvDiscriminatorPrior(N_dim=N_dim, ngpus=ngpus, dropout_p=dropout_p,
+        num_hidden_layers=num_hidden_layers, z_dim=z_dim)
+
+    print("encoder: {}\ndecoder: {}\ncategory discriminator: {}\nprior discriminator: {}".format(
+        str(encoder),
+        str(decoder),
+        str(discriminator_cat),
+        str(discriminator_prior)
+    ))
+    return encoder, decoder, discriminator_cat, discriminator_prior
