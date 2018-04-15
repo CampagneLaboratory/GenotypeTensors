73227c0219e61d01602e0e30dcc34eaf184c5587
Fabien Campagne <fac2003@campagnelab.org>
2018-04-15 14:06:19 -0400

    Reorganize search-hyperparameters to generate commands with SBI_SEARCH_PARAM_CONFIG.

--- bin/search-hyperparameters.sh
+++ bin/search-hyperparameters.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
-#set -x
-#echo ${GENOTYPE_TENSORS}
-#echo ${PYTHONPATH}
+set -x
+echo "usage: search-hyperparameters [num-models] --mode semisupervised|supervised-direct|supervised-mixup --problem genotyping:basename --num-training N ..."
+num_executions=$1
+shift
+if [ -z "${SBI_SEARCH_PARAM_CONFIG+set}" ]; then
+    echo "Variable SBI_SEARCH_PARAM_CONFIG not defined, you must specify a command file manually."
+    OPTION_COMMAND=" "
+else
+    COMMANDS="gen-args-${RANDOM}.txt"
+    arg-generator.sh 1g --config ${SBI_SEARCH_PARAM_CONFIG} --output ${COMMANDS} --num-commands ${num_executions}
+    OPTION_COMMAND="--commands ${COMMANDS}"
+fi
 
-python "${GENOTYPE_TENSORS}/src/org/campagnelab/dl/genotypetensors/autoencoder/SearchHyperparameters.py" "$@"
+
+python "${GENOTYPE_TENSORS}/src/org/campagnelab/dl/genotypetensors/autoencoder/SearchHyperparameters.py" "$@" ${OPTION_COMMAND}
