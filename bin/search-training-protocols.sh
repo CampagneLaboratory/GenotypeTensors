51f56f42baf4652ce46dca98a091bac93a763e0b
Fabien Campagne <fac2003@campagnelab.org>
2018-04-15 21:49:18 -0400

    Adjust number of models per GPU for each training protocol. Make sure indel weight factor is 1-10 for each training protocol.

--- bin/search-training-protocols.sh
+++ bin/search-training-protocols.sh
@@ -13,12 +13,12 @@ export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/initial-search-hyperpa
 export CUDA_VISIBLE_DEVICES=2
 mkdir -p search/supervised/mixup
 (cd search/supervised/mixup
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup --num-workers 4  --num-models-per-gpu 32 > out.log &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup --num-workers 4  --num-models-per-gpu 16 > out.log &
 )
 
 export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/initial-search-hyperparam-semisupervised-autoencoder.txt
 export CUDA_VISIBLE_DEVICES=4
 mkdir -p search/semisupervised/aae
 (cd search/semisupervised/aae
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 32 > out.log &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 16 > out.log &
 )
