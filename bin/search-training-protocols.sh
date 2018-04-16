cc21e52de57aa98eaabb9a5b487f99ac1ddbbd80
Fabien Campagne <fac2003@campagnelab.org>
2018-04-15 21:36:50 -0400

    Adjust script.

--- bin/search-training-protocols.sh
+++ bin/search-training-protocols.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
+. `dirname "${BASH_SOURCE[0]}"`/setup.sh
+
+PARAMS="--max-epochs 100 "
 export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel.txt
 export CUDA_VISIBLE_DEVICES=0
 mkdir -p search/supervised/direct
 (cd search/supervised/direct
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:CNG-NA12878-realigned-2018-01-30 --mode supervised-direct --num-workers 4  --num-models-per-gpu 32 &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_direct --num-workers 4  --num-models-per-gpu 32 > out.log &
 )
 
-export SBI_SEARCH_PARAM_CONFIG=config/initial-search-hyperparam-supervised-funnel-mixup.txt
+export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/initial-search-hyperparam-supervised-funnel-mixup.txt
 export CUDA_VISIBLE_DEVICES=2
 mkdir -p search/supervised/mixup
 (cd search/supervised/mixup
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:CNG-NA12878-realigned-2018-01-30 --mode supervised-direct --num-workers 4  --num-models-per-gpu 32 &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup --num-workers 4  --num-models-per-gpu 32 > out.log &
 )
 
-export SBI_SEARCH_PARAM_CONFIG=config/initial-search-hyperparam-semisupervised-autoencoder.txt
+export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/initial-search-hyperparam-semisupervised-autoencoder.txt
 export CUDA_VISIBLE_DEVICES=4
 mkdir -p search/semisupervised/aae
 (cd search/semisupervised/aae
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 32 &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 32 > out.log &
 )
