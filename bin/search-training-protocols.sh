0a2bafdc0a01ac0d5600b6d6a141f48fed52779b
Fabien Campagne <fac2003@campagnelab.org>
2018-04-18 11:43:00 -0400

    Prepare for AAE run.

--- bin/search-training-protocols.sh
+++ bin/search-training-protocols.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-PARAMS="--max-epochs 100 --num-workers 2 --num-models-per-gpu 64  "
+PARAMS="--max-epochs 100 --num-workers 4 --num-models-per-gpu 64  "
 #export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel.txt
 #export CUDA_VISIBLE_DEVICES=0
 #mkdir -p search/supervised/direct
 #(cd search/supervised/direct
 #${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_direct   ${PARAMS} > out.log &
 #)
-#
+
 #export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel-mixup.txt
 #export CUDA_VISIBLE_DEVICES=2
 #mkdir -p search/supervised/mixup
 #(cd search/supervised/mixup
 #${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup ${PARAMS}> out.log &
 #)
-#
-#export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-autoencoder.txt
-#export CUDA_VISIBLE_DEVICES=4
-#mkdir -p search/semisupervised/aae
-#(cd search/semisupervised/aae
-#${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS}> out.log &
-#)
-#
 
-export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-mixup.txt
+export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-autoencoder.txt
 export CUDA_VISIBLE_DEVICES=4
-mkdir -p search/semisupervised/mixup
-(cd search/semisupervised/mixup
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS}   > out.log &
+mkdir -p search/semisupervised/aae
+(cd search/semisupervised/aae
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS} --num-models-per-gpu 32 > out.log &
 )
+
+#
+#export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-mixup.txt
+#export CUDA_VISIBLE_DEVICES=4
+#mkdir -p search/semisupervised/mixup
+#(cd search/semisupervised/mixup
+#${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS}   > out.log &
+#)
