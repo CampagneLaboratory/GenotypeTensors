1c69721b246c510f2ebac5dd774800967553e3c3
Fabien Campagne <fac2003@campagnelab.org>
2018-04-18 11:41:28 -0400

    Remove --epoch-min-accuracy for AAE because these models tend to yield much lower performance and we may loose too many data points if restricted in this way.

--- bin/search-training-protocols.sh
+++ bin/search-training-protocols.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 . `dirname "${BASH_SOURCE[0]}"`/setup.sh
 
-PARAMS="--max-epochs 100 "
-export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel.txt
-export CUDA_VISIBLE_DEVICES=0
-mkdir -p search/supervised/direct
-(cd search/supervised/direct
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_direct --num-workers 4  --num-models-per-gpu 32 > out.log &
-)
-
-export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel-mixup.txt
-export CUDA_VISIBLE_DEVICES=2
-mkdir -p search/supervised/mixup
-(cd search/supervised/mixup
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup --num-workers 4  --num-models-per-gpu 1 > out.log &
-)
-
-export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-autoencoder.txt
-export CUDA_VISIBLE_DEVICES=4
-mkdir -p search/semisupervised/aae
-(cd search/semisupervised/aae
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 1 > out.log &
-)
-
+PARAMS="--max-epochs 100 --num-workers 2 --num-models-per-gpu 64  "
+#export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel.txt
+#export CUDA_VISIBLE_DEVICES=0
+#mkdir -p search/supervised/direct
+#(cd search/supervised/direct
+#${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_direct   ${PARAMS} > out.log &
+#)
+#
+#export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-supervised-funnel-mixup.txt
+#export CUDA_VISIBLE_DEVICES=2
+#mkdir -p search/supervised/mixup
+#(cd search/supervised/mixup
+#${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode supervised_mixup ${PARAMS}> out.log &
+#)
+#
+#export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-autoencoder.txt
+#export CUDA_VISIBLE_DEVICES=4
+#mkdir -p search/semisupervised/aae
+#(cd search/semisupervised/aae
+#${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS}> out.log &
+#)
+#
 
 export SBI_SEARCH_PARAM_CONFIG=${GENOTYPE_TENSORS}/config/final-search-hyperparam-semisupervised-mixup.txt
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=4
 mkdir -p search/semisupervised/mixup
 (cd search/semisupervised/mixup
-${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised --num-workers 4  --num-models-per-gpu 1 > out.log &
+${GENOTYPE_TENSORS}/bin/search-hyperparameters.sh 64 --problem genotyping:../../../CNG-NA12878-realigned-2018-01-30 --mode semisupervised  ${PARAMS}   > out.log &
 )
