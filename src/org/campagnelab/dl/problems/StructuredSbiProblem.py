742eba3dc2aaac08bf011ae1a6d18f8bf357b0b1
Fabien Campagne <fac2003@campagnelab.org>
2018-05-05 10:34:28 -0400

    Fix collate_sbi function. The function must return a tuple (example_indices, data_map).

--- src/org/campagnelab/dl/problems/StructuredSbiProblem.py
+++ src/org/campagnelab/dl/problems/StructuredSbiProblem.py
@@ -17,17 +17,23 @@ def collate_sbi(batch):
     """ For each batch, organize sbi messages in a list, returned as first element of the batch tuple"""
     element = batch[0]
 
-    if isinstance(element[0],dict)  and element[0]['type']=="BaseInformation":
+    if isinstance(element[0], dict) and element[0]['type'] == "BaseInformation":
         # note that batch_element[1][1] drops the index of the example: batch_element[1][0] to
         # keep only softmaxGenotype and metaData.
-        return {'sbi':[batch_element[0] for batch_element in batch]}, \
-               default_collate([ batch_element[1][1] for batch_element in batch])
+        example_indices = default_collate([batch_element[1][0] for batch_element in batch])
+        data_map = {"sbi":[batch_element[0] for batch_element in batch]}
+        data_map.update(   default_collate([batch_element[1][1] for batch_element in batch]))
+
+
+        return example_indices, data_map
     else:
         return default_collate(batch)
+
 
 class StructuredSbiGenotypingProblem(SbiProblem):
     """An SBI problem where the tensors are generated from structured messages directly from an SBI, and
     labels are loaded from the vec file. """
+
     # TODO: overwrite the dataset and loader methods to use StructuredGenotypeDataset
     def name(self):
         return self.basename_prefix() + self.basename
@@ -39,10 +45,10 @@ class StructuredSbiGenotypingProblem(SbiProblem):
         return []
 
     def get_vector_names(self):
-        return [ "softmaxGenotype","metaData"]
+        return ["softmaxGenotype", "metaData"]
 
     def train_set(self):
-        return StructuredGenotypeDataset(self.basename+"-train")
+        return StructuredGenotypeDataset(self.basename + "-train")
 
     def validation_set(self):
         return StructuredGenotypeDataset(self.basename + "-validation")
@@ -54,9 +60,9 @@ class StructuredSbiGenotypingProblem(SbiProblem):
         return StructuredGenotypeDataset(self.basename + "-unlabeled")
 
     def loader_for_dataset(self, dataset, shuffle=False):
-        return iter(DataLoader(dataset=dataset, shuffle=False, batch_size=self.mini_batch_size(),collate_fn=lambda batch:collate_sbi(batch),
+        return iter(DataLoader(dataset=dataset, shuffle=False, batch_size=self.mini_batch_size(),
+                               collate_fn=lambda batch: collate_sbi(batch),
                                num_workers=self.num_workers, pin_memory=False, drop_last=self.drop_last_batch))
-
 
     def get_output_names(self):
         return ["softmaxGenotype"]
