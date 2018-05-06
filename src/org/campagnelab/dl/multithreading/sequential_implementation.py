6f178f16735210b54314c8bb18bd536258374343
Fabien Campagne <fac2003@campagnelab.org>
2018-05-06 11:05:18 -0400

    Do not call cuda() on non-variables or non-tensors.

--- src/org/campagnelab/dl/multithreading/sequential_implementation.py
+++ src/org/campagnelab/dl/multithreading/sequential_implementation.py
@@ -143,7 +143,8 @@ class CpuGpuDataProvider(DataProvider):
             batch = batch_data[batch_name]
             for var_name in batch.keys():
                 if not self.fake_gpu_on_cpu:
-                    batch[var_name] = batch[var_name].cuda()
+                    if hasattr(batch[var_name],"cuda"):
+                        batch[var_name] = batch[var_name].cuda()
         self.gpu_batches_queue.put((batch_indices, batch_data), block=True)
 
 
